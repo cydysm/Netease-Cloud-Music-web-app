eedf828255caa5226fa0fe78827c80eab10930f5
irrelephant <irrelephant@wakfo.com>
2019-05-28 13:35:54 +0800

    add comment view

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+// import Home from './views/Home.vue';
+import NeteaseMusicComment from './views/NeteaseMusicComment.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home
+    // },
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'neteaseMusicComment',
+      component: NeteaseMusicComment,
     },
     // {
     //   path: '/about',
@@ -17,7 +23,7 @@ export default new Router({
     //   // route level code-splitting
     //   // this generates a separate chunk (about.[hash].js) for this route
     //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    //   component: () => import(/* webpackChunkName: 'about' */ './views/About.vue'),
     // },
   ],
 });
---
+++ src/views/NeteaseMusicComment.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="netease-music-comment">
+    <button @click="fetchData">test</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'neteaseMusicComment',
+  data() {
+    return {
+      randomCommentUrl: 'http://meaningless.cn/ap1/netease_music/song_random_comment',
+      devRandomCommentUrl: 'http://127.0.0.1:5000/netease_music/song_random_comment',
+      content: {},
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.axios({
+        method: 'GET',
+        url: this.devRandomCommentUrl,
+        // url: this.randomCommentUrl,
+      }).then((resp) => {
+        // console.log(resp.data);
+        Object.assign(this.content, resp.data.data);
+        console.log(this.content);
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
